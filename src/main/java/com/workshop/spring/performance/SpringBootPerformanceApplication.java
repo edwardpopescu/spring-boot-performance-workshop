cccc6e0ec972426d0c16bac95829d311a4c09f94
Edward Popescu <edward.popescu@ing.com>
2023-05-29 22:41:28 +0300

    initial commit and project setup

---
+++ src/main/java/com/workshop/spring/performance/SpringBootPerformanceApplication.java
@@ -0,0 +1,13 @@
+package com.workshop.spring.performance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPerformanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPerformanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workshop/spring/performance/SpringBootPerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.spring.performance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPerformanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
