65bf98f09ec2b68c0d88ef5ff844b22fb17ce999
Edward Popescu <edward.popescu@ing.com>
2023-06-29 18:10:32 +0200

    add reactive mongo repos annotation

--- app/src/main/java/com/workshop/spring/performance/SpringBootPerformanceApplication.java
+++ app/src/main/java/com/workshop/spring/performance/SpringBootPerformanceApplication.java
@@ -2,8 +2,10 @@ package com.workshop.spring.performance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
+@EnableReactiveMongoRepositories
 public class SpringBootPerformanceApplication {
 
 	public static void main(String[] args) {
